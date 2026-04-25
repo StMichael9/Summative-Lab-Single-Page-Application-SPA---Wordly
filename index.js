917e4644ea4aff284c2e765aa26076777e74f1a8
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-24 22:45:32 -0700

    Everything is working so far time to add fetch logic

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+// Added api
+const api = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
 const handleSearch = (event) => {
   event.preventDefault();
   const getSearch = document.getElementById("search-input");
   const search = getSearch.value;
   console.log("User searched for:", search);
+  fetchWord(search);
 };
 
 const form = document.getElementById("search-form");
 form.addEventListener("submit", handleSearch);
 
-const fetchWord = async (word) => {};
+const fetchWord = async (word) => {
+  console.log("Fetching..." /* Something will get passed here most likey*/);
+  const apiLink = api + word;
+  console.log(apiLink);
+};
