e7b0ad37c07fd40356e5e694df1ade204843680a
St Michael Egenamba <michaelegenamba@gmail.com>
2026-04-24 22:04:39 -0700

    Adding to github

---
+++ index.js
@@ -0,0 +1,9 @@
+const handleSearch = (event) => {
+  event.preventDefault();
+  const getSearch = document.getElementById("search-input");
+  const search = getSearch.value;
+  console.log("User searched for:", search);
+};
+
+const form = document.getElementById("search-form");
+form.addEventListener("submit", handleSearch);
